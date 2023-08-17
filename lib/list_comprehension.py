9ec2071bc01162e70e25dcc11c829073ec0f0b3f
whiplade <nmawira13@gmail.com>
2023-08-17 10:07:18 +0300

    Completed Assignment

--- lib/list_comprehension.py
+++ lib/list_comprehension.py
@@ -1,7 +1,9 @@
 #!/usr/bin/env python3
 
 def return_evens(num_list):
-    pass
+    even_nos = [num for num in num_list if num % 2 == 0]
+    return even_nos
 
 def make_exclamation(sentence_list):
-    pass
+    exclaimed_sentences = [sentence + '!' for sentence in sentence_list]
+    return exclaimed_sentences
